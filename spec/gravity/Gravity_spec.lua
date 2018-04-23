6b4df3418dabc7abeb3448935d1c46be75e1a903
Gabriel Velasco <gabriel.velasco001@gmail.com>
2018-04-23 13:06:39 -0400

    Added gravity code; need to make down collision logic

--- spec/gravity/Gravity_spec.lua
+++ spec/gravity/Gravity_spec.lua
@@ -3,13 +3,13 @@ describe('gravity', function()
   local collision = mach.mock_table({
     down = function() end,
   }, 'collision')
-  local gravity = require 'gravity.Gravity'(collision)
 
   local dude = {}
   local default_y = 10
-  local g_s = 9.8
-  local weight = 100
+  local gravities = (9.8)
   local delta_time = 1
+
+  local gravity = require 'gravity.Gravity'(collision, gravities)
 
   before_each(function()
     dude.y = default_y
@@ -18,7 +18,30 @@ describe('gravity', function()
 
   it('should not fall if moving is on top of something', function()
     local stationary = { 'some stationary object' }
-    gravity(dude, stationary, delta_time)
+    collision.down:should_be_called_with(dude, stationary[1]):and_will_return(true):
+      when(function()
+        gravity(dude, stationary, delta_time)
+      end)
     assert.equal(default_y, dude.y)
+  end)
+
+  it('should fall if moving is not on top of something', function()
+    local stationary = { 'some stationary object' }
+    collision.down:should_be_called_with(dude, stationary[1]):and_will_return(false):
+      when(function()
+        gravity(dude, stationary, delta_time)
+      end)
+    assert.equal(default_y - (gravities * (delta_time ^ 2)), dude.y)
+  end)
+
+  it('should not fall if colliding against one of many objects', function()
+    local stationary = { 'list', 'of', 'objects' }
+    collision.down:may_be_called_with(dude, stationary[1]):and_will_return(false):
+      and_also(collision.down:may_be_called_with(dude, stationary[2]):and_will_return(true)):
+      and_also(collision.down:may_be_called_with(dude, stationary[3]):and_will_return(false)):
+      when(function()
+        gravity(dude, stationary, delta_time)
+      end)
+    assert.equal(default_y - (gravities *(delta_time ^ 2)), dude.y)
   end)
 end)
--- src/gravity/Gravity.lua
+++ src/gravity/Gravity.lua
@@ -1,5 +1,17 @@
-return function(collision)
-  return function()
+return function(collision, gravities)
+  local function not_colliding_with_something(moving, stationary)
+    for _, object in pairs(stationary) do
+      if not collision.down(moving, object) then
+        return true
+      end
+    end
 
+    return false
+  end
+
+  return function(moving, stationary, delta_time)
+    if not_colliding_with_something(moving, stationary) then
+      moving.y = moving.y - (gravities * (delta_time ^ 2))
+    end
   end
 end
